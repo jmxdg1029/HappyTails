13706a5beb615c658fb96426a8d860427ee5aeca
John Michael De Guzman <jmxdg1029@gmail.com>
2025-08-24 22:25:50 -0400

    Contact Page complete

--- happy-tail/src/components/ContactSection.js
+++ happy-tail/src/components/ContactSection.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import '../components/ContactSection.scss';
 import Swal from 'sweetalert2'
 
@@ -33,8 +34,8 @@ const ContactSection = () => {
             <div className="contact-section">
                 <div className="col-md-6 mt-3">
                     <form onSubmit={onSubmit}>
-                            <h2>Contact Form</h2>
-                            <h3>Please provide your dog's breed and a date you'd like to book</h3>
+                            <h2>CONTACT US</h2>
+                            <h4>Ready to book? Feel free to provide your dog's breed and the date you'd like to book</h4>
                                 <div className='input-box'>
                                     <label>Full Name</label>
                                     <input type="text" className="field" placeholder='Enter your name' name='name' required />
@@ -51,7 +52,14 @@ const ContactSection = () => {
                     </form>
                </div>
                <div className='col-md-6'>
-
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2864.0703473907524!2d-79.59796208708731!3d44.12317112178573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84eea19fb9dff979%3A0x5f3b41fe6cb4f546!2sHappy%20Tails%20Pet%20Sitting!5e0!3m2!1sen!2sca!4v1756087992674!5m2!1sen!2sca" 
+                    width="900" 
+                    height="850" 
+                    style={{border:"0"}}
+                     allowfullscreen="" 
+                     loading="lazy" 
+                     referrerpolicy="no-referrer-when-downgrade">
+                     </iframe>
                </div>
             </div> 
         );
